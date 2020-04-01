98253263404599ab5109428369e5f28c23f78de1
Marc <Garridocho@gmail.com>
2020-04-01 22:04:53 +0200

    Preus vivenda ciutats més importants de Catalunya.

--- Evolució preus vivenda.py
+++ Evolució preus vivenda.py
@@ -3,7 +3,7 @@
 
 # ## Web scraping
 
-# In[9]:
+# In[1]:
 
 
 import bs4 as bs
@@ -15,6 +15,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import io
+
+
+# In[2]:
+
+
+HTML = "https://www.trovimap.com/precio-vivienda/barcelona"
+
+
+# In[6]:
+
+
+# Amb .read_html() podem obtenir fàcilment la taula que ens mostra la web.
+
+df = pd.read_html(HTML)
+df[0].head()
+
+
+# In[7]:
+
+
+# Amb BeautifulSoup, podrem obtenir tot el String del codi HTML per si volem obtenir més informació.
+
+historypage = urllib.request.urlopen(HTML)
+soup = bs.BeautifulSoup(historypage,'html.parser')
+makeitastring = ''.join(map(str, soup))
+
+soup
+#makeitastring
+
+
+# In[5]:
+
 
 
 #retorna un llistat de ciutats i la seva variació de preu segons el html ja llegit per BeatifulSoup que li hem passat. Aquest html està format segons 
@@ -32,7 +64,7 @@ def obtenirCiutatsiVariacio(htmlSoup):
         cells = row.findAll('td')    
         link = cells[0].find('a')
         ciutat = link.find(text=True)
-        print (ciutat)
+        #print(ciutat)
         varMensual = cells[1].find(text=True)
         var3mesos = cells[2].find(text=True)
         varAnual = cells[3].find(text=True)
@@ -43,40 +75,79 @@ def obtenirCiutatsiVariacio(htmlSoup):
         
     return llistaVarCiutats
 
-# In[10]:
-
 
 HTML = "https://www.trovimap.com/precio-vivienda/barcelona"
-
-
-# In[11]:
-
-
-# Amb .read_html() podem obtenir fàcilment la taula que ens mostra la web.
-
-df = pd.read_html(HTML)
-df[0].head()
-
-
-# In[12]:
-
-
-# Amb BeautifulSoup, podrem obtenir tot el String del codi HTML per si volem obtenir més informació.
-
-df = pd.DataFrame()
-
-historypage = urllib.request.urlopen(HTML)
-soup = bs.BeautifulSoup(historypage,'html.parser')
-makeitastring = ''.join(map(str, soup))
 
 VarCiutat = obtenirCiutatsiVariacio(soup)
 
 print(VarCiutat[0])
 
+
 # In[ ]:
 
 
 
+
+
+# In[ ]:
+
+
+
+
+
+# In[13]:
+
+
+HTML = "https://www.trovimap.com/precio-vivienda/"
+
+
+# In[19]:
+
+
+HTMLEspana = HTML + "espana"
+
+df = pd.read_html(HTMLEspana)
+dfProvinciesEspana = df[0]
+dfProvinciesEspana = dfProvinciesEspana.rename(columns={"Unnamed: 0": 'Provincies'})
+
+dfProvinciesEspana.head()
+
+
+# ### Obtenim una taula amb la variació menusal de cada provincia de l'estat.
+
+# ### Si volem obtenir una taula, tenint per files les ciutats més importants de cada provincia tal com hem fet anteriorment amb la de Barcelona:
+
+# In[25]:
+
+
+dfProvinciesEspana.iloc[0]['Provincies']
+
+
+# ### Si volem obtenir una taula, tenint per files les ciutats més importants de Catalunta:
+
+# In[37]:
+
+
+# Agafar la taula per cada una de les quatre provincies de Catalunya.
+dfBarcelona = pd.read_html(HTML+"barcelona")
+dfTarragona = pd.read_html(HTML+"tarragona")
+dfGirona = pd.read_html(HTML+"girona")
+dfLleida = pd.read_html(HTML+"lleida")
+dfBarcelona = dfBarcelona[0]
+dfTarragona = dfTarragona[0]
+dfGirona = dfGirona[0]
+dfLleida = dfLleida[0]
+
+# Concatenar Taules.
+dfCatalunya = pd.concat([dfBarcelona, dfTarragona], ignore_index=True)
+dfCatalunya = pd.concat([dfCatalunya, dfGirona], ignore_index=True)
+dfCatalunya = pd.concat([dfCatalunya, dfLleida], ignore_index=True)
+
+# Renombrar columnes.
+dfCatalunya = dfCatalunya.rename(columns={"Unnamed: 0": 'Provincies', "Variación Mensual": 'Variació mensual',
+                                "Variación 3 meses": 'Variació tres messos', "Variación anual": 'Variació anual'})
+
+dfCatalunya.tail()
 
 
 # In[ ]:
