dbe8402e4f944d2702b89e8fccb81f6b44051089
Marc <Garridocho@gmail.com>
2020-04-02 23:19:51 +0200

    Extraure les ciutats més importants tant de Catalunya com d'España.

--- Evolució preus vivenda.py
+++ Evolució preus vivenda.py
@@ -3,7 +3,7 @@
 
 # ## Web scraping
 
-# In[1]:
+# In[4]:
 
 
 import bs4 as bs
@@ -17,7 +17,7 @@ import seaborn as sns
 import io
 
 
-# In[2]:
+# In[5]:
 
 
 HTML = "https://www.trovimap.com/precio-vivienda/barcelona"
@@ -41,11 +41,11 @@ historypage = urllib.request.urlopen(HTML)
 soup = bs.BeautifulSoup(historypage,'html.parser')
 makeitastring = ''.join(map(str, soup))
 
-soup
+#soup
 #makeitastring
 
 
-# In[5]:
+# In[8]:
 
 
 
@@ -95,13 +95,13 @@ print(VarCiutat[0])
 
 
 
-# In[13]:
+# In[9]:
 
 
 HTML = "https://www.trovimap.com/precio-vivienda/"
 
 
-# In[19]:
+# In[10]:
 
 
 HTMLEspana = HTML + "espana"
@@ -117,7 +117,7 @@ dfProvinciesEspana.head()
 
 # ### Si volem obtenir una taula, tenint per files les ciutats més importants de cada provincia tal com hem fet anteriorment amb la de Barcelona:
 
-# In[25]:
+# In[11]:
 
 
 dfProvinciesEspana.iloc[0]['Provincies']
@@ -125,7 +125,7 @@ dfProvinciesEspana.iloc[0]['Provincies']
 
 # ### Si volem obtenir una taula, tenint per files les ciutats més importants de Catalunta:
 
-# In[37]:
+# In[12]:
 
 
 # Agafar la taula per cada una de les quatre provincies de Catalunya.
@@ -144,14 +144,67 @@ dfCatalunya = pd.concat([dfCatalunya, dfGirona], ignore_index=True)
 dfCatalunya = pd.concat([dfCatalunya, dfLleida], ignore_index=True)
 
 # Renombrar columnes.
-dfCatalunya = dfCatalunya.rename(columns={"Unnamed: 0": 'Provincies', "Variación Mensual": 'Variació mensual',
+dfCatalunya = dfCatalunya.rename(columns={"Unnamed: 0": 'Ciutat', "Variación Mensual": 'Variació mensual',
                                 "Variación 3 meses": 'Variació tres messos', "Variación anual": 'Variació anual'})
 
 dfCatalunya.tail()
 
 
-# In[ ]:
+# -------
+
+# ### Ara volem treure totes les ciutats importants de España. Per fer això necesitem totes les url de cada provincia  a partir de el html.
+
+# In[43]:
 
 
+historypage = urllib.request.urlopen(HTMLEspana)
+soup = bs.BeautifulSoup(historypage,'html.parser')
+makeitastring = ''.join(map(str, soup))
+#soup
+#makeitastring
+
+# Buscar el patró dins del html on es guarda els noms de les provincies per despres buscar-les en url. 
+capitals = '/'.join(map(str, re.findall("precio-vivienda(.+?)\">",makeitastring)))
+capitals = capitals[46:570]
+print(capitals)
+
+# Ja tenim un array amb tots els noms de les provincies que haurem de posar a la url.
+capitalsArray = capitals.split('//')
+capitalsArray
 
 
+# In[48]:
+
+
+HTML = "https://www.trovimap.com/precio-vivienda/"
+HTML + capitalsArray[25]
+
+df = pd.read_html(HTML + capitalsArray[25])
+df[0].head()
+
+
+# In[64]:
+
+
+# Veiem que serveix, ara només hem de posar-lo en un loop per obtenir totes les ciutats importants d'España.
+dfEspana = pd.DataFrame()
+for capital in capitalsArray:
+    if capital != "ceuta" and capital != "soria":
+        # Per a cada url agafar la taula
+        dfAux = pd.read_html(HTML+capital)
+        dfAux = dfAux[0]
+        #Anar concatenant ciutats
+        dfEspana = pd.concat([dfEspana, dfAux], ignore_index=True)
+        print(capital)
+    
+    
+# Renombrar columnes.
+dfEspana = dfEspana.rename(columns={"Unnamed: 0": 'Ciutat', "Variación Mensual": 'Variació mensual',
+                                "Variación 3 meses": 'Variació tres messos', "Variación anual": 'Variació anual'})
+    
+dfEspana.tail()
+
+
+# ### Ja tenim taules amb les variacions econòmiques de les ciutats més importants tant de Catalunya com d'España.
+
+# ---------
